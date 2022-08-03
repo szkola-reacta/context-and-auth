25536aab5901f5872453a4fcfde1a2790111db9f
Patrycjusz Omiotek <patrycjusz.omiotek@ramp.network>
2022-08-03 20:56:17 +0200

    before authentication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+// import { Hello } from './Hello';
+import { Hello } from './HelloFn';
+import { ErrorBoundary } from './ErrorBoundary';
+import { Provider as UserProvider } from './UserContext';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ErrorBoundary>
+        <UserProvider>
+          <Hello />
+        </UserProvider>
+      </ErrorBoundary>
     </div>
   );
 }
